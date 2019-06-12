b3913586bb64768178fccbf14e3863779c6f6ed3
Ukidabek <ukidabek@gmail.com>
2019-06-12 20:47:26 +0200

    - SimpleBoxHero.prefab and animations added. - Reload added.

---
+++ Assets/Weapons/Logic/MagazineAction.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Weapons;
+
+public class MagazineAction : WeaponAction
+{
+    [SerializeField] private int _maxSize = 7;
+    [SerializeField] private int _counter = 0;
+
+    public UnityEvent OnEmptyCallback = new UnityEvent();
+
+    private void Awake()
+    {
+        _counter = _maxSize;
+    }
+
+    public override bool Perform()
+    {
+        if(--_counter == 0)
+        {
+            _counter = _maxSize;
+            OnEmptyCallback.Invoke();
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ Assets/Weapons/Logic/WaitForSecondsWeaponValidator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Weapons;
+
+public class WaitForSecondsWeaponValidator : WeaponValidator
+{
+    [SerializeField] private float _timeToWait = 1f;
+    [SerializeField] private float _counter = 0;
+
+    private void Awake()
+    {
+        enabled = false;
+    }
+
+    public override bool Validate()
+    {
+        return _counter <= 0;
+    }
+
+    private void OnEnable()
+    {
+        _counter = _timeToWait;
+    }
+
+    private void Update()
+    {
+        _counter -= Time.deltaTime;
+        if (_counter <= 0) enabled = false;
+    }
+}
