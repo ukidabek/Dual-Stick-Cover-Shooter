bc5e98e8da86e263e8b0f92c0b6c6fd38ce1e05a
Ukidabek <ukidabek@gmail.com>
2019-08-13 20:29:37 +0200

    - AI implementation start

---
+++ Assets/Characters/AI/Agent/Agent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+public class Agent : MonoBehaviour
+{
+    public IMove Move { get; private set; }
+
+    private void Awake()
+    {
+        var gameObject = transform.root;
+        var properties = GetType().GetProperties(BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+        foreach (var item in properties)
+            item.SetValue(this, gameObject.GetComponentInChildren(item.PropertyType));
+    }
+}
---
+++ Assets/Characters/AI/FiniteStateMachine.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class State
+{
+    protected FiniteStateMachine stateMachine = null;
+    
+    public State(FiniteStateMachine stateMachine)
+    {
+        this.stateMachine = stateMachine;
+    }
+
+    public virtual void Enter() { }
+    public abstract Type Update();
+    public virtual void Exit() { }
+}
+
+public class MoveToRandomPointState : State
+{
+    public MoveToRandomPointState(FiniteStateMachine stateMachine) : base(stateMachine) { }
+
+    public override Type Update()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class IdleState : State
+{
+    private float idleTime = 1f;
+
+    private float counter = 0;
+
+    public IdleState(FiniteStateMachine stateMachine, float idleTime) : base(stateMachine)
+    {
+        this.idleTime = idleTime;
+    }
+
+    public override void Enter()
+    {
+        counter = idleTime;
+    }
+
+    public override Type Update()
+    {
+        counter -= Time.deltaTime;
+        if (counter <= 0)
+            return typeof(MoveToRandomPointState);
+        return null;
+    }
+}
+
+[RequireComponent(typeof(Agent))]
+public class FiniteStateMachine : MonoBehaviour
+{
+    [SerializeField] private Agent agent = null;
+    public Agent Agent { get => agent; }
+
+    private State CurrentState = null;
+
+    public void Update()
+    {
+        CurrentState?.Update();
+    }
+
+    private void Reset()
+    {
+        agent = gameObject.GetComponent<Agent>();
+    }
+}
