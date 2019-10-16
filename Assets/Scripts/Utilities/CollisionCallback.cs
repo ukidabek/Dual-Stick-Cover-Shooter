a24796ea50d976ad634d5232270d8824b938960b
Ukidabek <ukidabek@gmail.com>
2019-10-16 19:58:30 +0200

    Utilities sub repository added.

--- Assets/Scripts/Utilities/CollisionCallback.cs
+++
@@ -1,5 +0,0 @@
-﻿using System;
-using UnityEngine;
-using UnityEngine.Events;
-
-[Serializable] public class CollisionCallback : UnityEvent<Collision> { }
--- Assets/Scripts/Utilities/OnCollider.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using UnityEngine;
-using UnityEngine.Events;
-
-namespace Utilities.Events
-{
-	[Serializable] public class OnCollider : UnityEvent<Collider> { }
-}
