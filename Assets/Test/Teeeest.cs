d786193877fc065f1138439a78111ad3042be46a
Ukidabek <ukidabek@gmail.com>
2019-10-25 19:05:57 +0200

    Custom type class generator added.
    Logic for generating all necessary files for custom type object added.

--- Assets/Test/Teeeest.cs
+++
@@ -1,9 +0,0 @@
-﻿using CustomType;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Teeeest : MonoBehaviour
-{
-	[SerializeField] private SuperKlasa superKlasa = new SuperKlasa();
-}
