844014c4f39ae82f57e099d03c9d6814212756c6
Ukidabek <ukidabek@gmail.com>
2019-10-29 15:48:53 +0100

    PlayerSpawner and graphic updates.
    PlayerSpawner component added.
    Player prefab graphic updates (head for player added).
    Rifle prefab scaled down.

---
+++ Assets/Scripts/Characters/Player/PlayerSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Characters.Player
+{
+	public class PlayerSpawner : MonoBehaviour
+	{
+		private void OnEnable()
+		{
+			PlayerController.PlayerAdded += MovePlayer;
+			if (PlayerController.Characters != null)
+				foreach (PlayerController player in PlayerController.Characters)
+					player.transform.position = GetPositon(player);
+		}
+
+		private void OnDestroy()
+		{
+			PlayerController.PlayerAdded -= MovePlayer;
+		}
+
+		private Vector3 GetPositon(PlayerController player)
+		{
+			return this.transform.position;
+		}
+
+		private void Reset()
+		{
+			gameObject.name = GetType().Name;
+		}
+
+		private void MovePlayer(PlayerController player)
+		{
+			player.transform.position = GetPositon(player);
+		}
+	}
+}
